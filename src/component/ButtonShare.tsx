9889631d5ad9d00004eb3c36e04106a2cdb06332
ranto <rantoirwanda08@gmail.com>
2024-10-30 15:16:58 +0700

    nice

--- src/component/ButtonShare.tsx
+++ src/component/ButtonShare.tsx
@@ -6,16 +6,18 @@ const ButtonShare = () => {
     const imgUrl =
       "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
       const shareText = "bla bla bla";
+      const url: 'https://try-three-pearl.vercel.app/'
 
     try {
       const response = await fetch(imgUrl);
       const blob = await response.blob();
       const file = new File([blob], "event-image.jpg", { type: blob.type });
 
-      if (navigator.share && navigator.canShare({ files: [file], title: shareText  })) {
+      if (navigator.share && navigator.canShare({ files: [file], title: shareText, url: url  })) {
         await navigator.share({
           files: [file],
-          title: shareText
+          title: shareText,
+          url: url
         });
       }
     } catch (error) {
