9b995eb7ed82480f6b2a9481a5ce292fd09ba1f4
ranto <rantoirwanda08@gmail.com>
2024-10-30 15:10:10 +0700

    add share text

--- src/component/ButtonShare.tsx
+++ src/component/ButtonShare.tsx
@@ -5,15 +5,17 @@ const ButtonShare = () => {
   const handleShare = async () => {
     const imgUrl =
       "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
+      const shareText = "bla bla bla";
+
     try {
       const response = await fetch(imgUrl);
       const blob = await response.blob();
       const file = new File([blob], "event-image.jpg", { type: blob.type });
 
-      if (navigator.share && navigator.canShare({ files: [file] })) {
+      if (navigator.share && navigator.canShare({ files: [file], title: shareText  })) {
         await navigator.share({
           files: [file],
-          text: "bla bla bla",
+          title: shareText
         });
       }
     } catch (error) {
