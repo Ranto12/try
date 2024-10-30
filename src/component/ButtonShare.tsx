0c0bd92a01c5f56ec98e68f0d29c0c59e88a612b
ranto <rantoirwanda08@gmail.com>
2024-10-30 16:37:13 +0700

    cek ib bro

--- src/component/ButtonShare.tsx
+++ src/component/ButtonShare.tsx
@@ -5,7 +5,7 @@ const ButtonShare = () => {
   const handleShare = async () => {
     const imgUrl =
       "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
-      const shareText = `Yuk ikut event:  ${process.env.NEXT_PUBLIC_URL}/event/detail/1`;
+      const shareText = `Yuk ikut event:  https://try-three-pearl.vercel.app//event/detail/1`;
       const url = `${process.env.NEXT_PUBLIC_URL}/event/detail/1`
 
     try {
@@ -13,11 +13,11 @@ const ButtonShare = () => {
       const blob = await response.blob();
       const file = new File([blob], "event-image.jpg", { type: blob.type });
 
-      if (navigator.share && navigator.canShare({ files: [file], title: shareText, url: url  })) {
+      if (navigator.share && navigator.canShare({ files: [file], title: shareText, url: shareText  })) {
         await navigator.share({
           files: [file],
           title: shareText,
-          url: url,
+          url: shareText,
         });
       }
     } catch (error) {
