99d8d39499204d07ab0bf0315b1789075ff5db9f
ranto <rantoirwanda08@gmail.com>
2025-01-14 13:00:07 +0700

    nice

--- src/component/ButtonShare.tsx
+++ src/component/ButtonShare.tsx
@@ -4,7 +4,7 @@ const ButtonShare = () => {
   const handleShare = async () => {
     const imgUrl =
       "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
-    const shareText = `Yuk ikut event: ${process.env.NEXT_PUBLIC_URL}/event/detail/1`;
+    const shareText = `Yuk ikut event ${process.env.NEXT_PUBLIC_URL}/event/detail/1`;
     const url = `${process.env.NEXT_PUBLIC_URL}/event/detail/1`;
 
     try {
@@ -13,17 +13,15 @@ const ButtonShare = () => {
       const file = new File([blob], "event-image.jpg", { type: blob.type });
 
       if (navigator.share) {
-        // Check if the device supports sharing with files
         if (navigator.canShare && navigator.canShare({ files: [file] })) {
           await navigator.share({
             files: [file],
-            url,
+            url: shareText,
           });
         } else {
-          // Share only text and URL if files are not supported
           await navigator.share({
             text: shareText,
-            url,
+            url: shareText,
           });
         }
       } else {
