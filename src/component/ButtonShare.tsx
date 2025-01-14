aa49b8164e001fe0c366d7dea8a0b8cabac3acab
ranto <rantoirwanda08@gmail.com>
2025-01-14 23:08:36 +0700

    nice bro

--- src/component/ButtonShare.tsx
+++ src/component/ButtonShare.tsx
@@ -1,51 +1,38 @@
 "use client";
 
 const ButtonShare = () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.platform);
+console.log(isMobile)
+  const img = "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
   const handleShare = async () => {
-    const imgUrl =
-      "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
-    const eventUrl = `${process.env.NEXT_PUBLIC_URL}/event/detail/1`;
-    const title = "Lihat detail event kami";
-    const text = `${title} di ${eventUrl}`;
-
+    const imgUrl = img;
+    const shareText = `Yuk ikut event: `;
     try {
-      // Check if sharing to WhatsApp
-      const isWhatsApp = navigator.userAgent.toLowerCase().includes("whatsapp");
-      if (isWhatsApp) {
-        // Share via WhatsApp (title and URL only)
-        const waUrl = `https://wa.me/?text=${encodeURIComponent(text)}`;
-        window.open(waUrl, "_blank");
-        return;
-      }
-
-      // If not WhatsApp, use Web Share API
       const response = await fetch(imgUrl);
       const blob = await response.blob();
-      const file = new File([blob], "event-image.jpg", { type: blob.type });
+      const file = new File([blob], 'event-image.jpg', { type: blob.type });
 
-      if (navigator.share) {
-        if (
-          navigator.canShare &&
-          navigator.canShare({ files: [file], text, title, url: eventUrl })
-        ) {
+      if (
+        navigator.share &&
+        navigator.canShare({ files: [file], title: shareText, url: shareText, text: shareText })
+      ) {
+        if (isMobile) {
           await navigator.share({
             files: [file],
-            title,
-            text,
-            url: eventUrl,
+            url: img,
+            text: shareText,
+            title: shareText
           });
         } else {
           await navigator.share({
-            title,
-            text,
-            url: eventUrl,
+            url: img,
+            text: shareText,
+            title: shareText
           });
         }
-      } else {
-        alert("Web Share API tidak didukung di perangkat ini.");
       }
     } catch (error) {
-      console.error("Error saat mencoba berbagi:", error);
+      throw error;
     }
   };
 
