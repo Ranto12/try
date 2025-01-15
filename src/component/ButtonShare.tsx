0ab00fbbfd41c262ad197db18275174617292ba3
ranto <rantoirwanda08@gmail.com>
2025-01-15 10:35:01 +0700

    nice

--- src/component/ButtonShare.tsx
+++ src/component/ButtonShare.tsx
@@ -4,11 +4,12 @@ const ButtonShare = () => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.platform);
 console.log(isMobile)
   const img = "https://storage.googleapis.com/crm-go/REVAMP-LOYALTY/714c1700-288e-41d5-9dcf-e9e215bdf496.jpg";
+  const shareText = `Yuk ikut event: https://loyalty-kalbe-family-dev-chdcaf35ya-et.a.run.app/event/detail/28`;
   const handleShare = async () => {
-    const imgUrl = img;
-    const shareText = `Yuk ikut event: `;
+    // const imgUrl = event?.shareImageUrl || `${getCurrentURL()}/images/brands/kpoin.png`;
+    // const shareText = `Yuk ikut event: ${event?.eventName} ${currentUrl}`;
     try {
-      const response = await fetch(imgUrl);
+      const response = await fetch(img);
       const blob = await response.blob();
       const file = new File([blob], 'event-image.jpg', { type: blob.type });
 
@@ -16,20 +17,12 @@ console.log(isMobile)
         navigator.share &&
         navigator.canShare({ files: [file], title: shareText, url: shareText, text: shareText })
       ) {
-        if (isMobile) {
-          await navigator.share({
-            files: [file],
-            url: img,
-            text: shareText,
-            title: shareText
-          });
-        } else {
-          await navigator.share({
-            url: img,
-            text: shareText,
-            title: shareText
-          });
-        }
+        await navigator.share({
+          files: [file],
+          url: decodeURI(shareText),
+          // text: shareText,
+          // title: shareText
+        });
       }
     } catch (error) {
       throw error;
